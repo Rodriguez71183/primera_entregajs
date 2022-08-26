808f3ee5b8a8207d933538806a535bb5177e2cea
danirodriguez71183@gmail.com <danirodriguez71183@gmail.com>
2022-08-25 21:06:44 -0300

    primera entrega

---
+++ complementario.js
@@ -0,0 +1,7 @@
+let numero =  Number(prompt("ingrese un numero del 1 al 10"))
+
+
+for(i = 0; i <=10; i++){
+    let resultado = numero * i
+    alert(` El resultado de ${numero} x ${i} es ${resultado}`)
+}
